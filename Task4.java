7581e05f60ab941d732abf7b179fc2cb5ac79e54
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-09-21 21:54:28 +0300

    задачи семинара

--- Task4.java
+++ Task4.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task4 {
     /*
 Реализуйте метод checkArray(Integer[] arr), принимающий в качестве аргумента целочисленный одномерный массив.
@@ -6,16 +9,27 @@ public class Task4 {
 2. Если null’ы встретились в нескольких ячейках, то идеально было бы все их “подсветить”
      */
     public static void main(String[] args) {
-        checkArray(new Integer[]{15, 466, null, null, 47});
+        checkArray2(new Integer[]{15, 466, null, null, 47});
     }
 
-    public static void checkArray(Integer[] arr) {
+    public static void checkArray1(Integer[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == null) {
                 throw new NullPointerException(String.format("В ячейке с индексом %s содержится null", i));
-
             }
+        }
+        System.out.println("Значение null не обнаружено");
+    }
 
+    public static void checkArray2(Integer[] arr) {
+        List<Integer> inexes = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                inexes.add(i);
+            }
+        }
+        if (!inexes.isEmpty()) {
+            throw new NullPointerException(String.format("В ячейках с индексами %s содержится null", inexes));
         }
         System.out.println("Значение null не обнаружено");
     }
